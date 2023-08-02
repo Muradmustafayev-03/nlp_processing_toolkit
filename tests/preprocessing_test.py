b28e332eb9a66e9454d7b6360358611091c22f2f
Murad <muradmustafayev03@gmail.com>
2023-08-02 11:10:28 +0400

    created test cases

---
+++ tests/preprocessing_test.py
@@ -0,0 +1,38 @@
+from src.preprocessing import *
+import unittest
+
+SENTENCES = [
+    'The sun set over the horizon, casting a golden hue on the tranquil sea.',
+    'Birds chirped merrily in the treetops, heralding the arrival of a new day.',
+    'With a gentle breeze rustling the leaves, the scent of blooming flowers filled the air.',
+    'Laughter echoed through the park as children played games and enjoyed the warm summer afternoon.',
+    'In the distance, a faint rumble of thunder warned of an approaching storm.'
+]
+
+FILTERED = [
+    'sun set horizon casting golden hue tranquil sea',
+    'birds chirped merrily treetops heralding arrival new day',
+    'gentle breeze rustling leaves scent blooming flowers filled air',
+    'laughter echoed park children played games enjoyed warm summer afternoon',
+    'distance faint rumble thunder warned approaching storm'
+]
+
+LEMMAS = [
+    'The sun set over the horizon, cast a golden hue on the tranquil sea.',
+    'Bird chirp merrily in the treetop, herald the arrival of a new day.',
+    'With a gentle breeze rustle the leaf, the scent of bloom flower fill the air.',
+    'Laughter echo through the park as child play game and enjoy the warm summer afternoon.',
+    'In the distance, a faint rumble of thunder warn of an approach storm.'
+]
+
+LEMMAS_FILTERED = [
+    'sun set horizon cast golden hue tranquil sea',
+    'bird chirp merrily treetop herald arrival new day',
+    'gentle breeze rustle leaf scent bloom flower fill air',
+    'laughter echo park child play game enjoy warm summer afternoon',
+    'distance faint rumble thunder warn approach storm'
+]
+
+
+if __name__ == '__main__':
+    unittest.main()

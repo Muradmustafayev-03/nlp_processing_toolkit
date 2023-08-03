b52fd4e192c3199fe996a5728a62589c1875735a
Murad <muradmustafayev03@gmail.com>
2023-08-03 13:15:18 +0400

    moved test constants to a separate file

--- tests/preprocessing_test.py
+++ tests/preprocessing_test.py
@@ -1,48 +1,7 @@
 from src.preprocessing import *
+from constants import *
 import unittest
 import random
-
-SENTENCES = [
-    'The sun set over the horizon, casting a golden hue on the tranquil sea.',
-    'Birds chirped merrily in the treetops, heralding the arrival of a new day.',
-    'With a gentle breeze rustling the leaves, the scent of blooming flowers filled the air.',
-    'Laughter echoed through the park as children played games and enjoyed the warm summer afternoon.',
-    'In the distance, a faint rumble of thunder warned of an approaching storm.'
-]
-
-FILTERED = [
-    'sun set horizon casting golden hue tranquil sea',
-    'birds chirped merrily treetops heralding arrival new day',
-    'gentle breeze rustling leaves scent blooming flowers filled air',
-    'laughter echoed park children played games enjoyed warm summer afternoon',
-    'distance faint rumble thunder warned approaching storm'
-]
-
-LEMMAS = [
-    'the sun set over the horizon , cast a golden hue on the tranquil sea .',
-    'bird chirp merrily in the treetop , herald the arrival of a new day .',
-    'with a gentle breeze rustle the leaf , the scent of bloom flower fill the air .',
-    'laughter echo through the park as child play game and enjoy the warm summer afternoon .',
-    'in the distance , a faint rumble of thunder warn of an approach storm .'
-]
-
-LEMMAS_FILTERED = [
-    'sun set horizon cast golden hue tranquil sea',
-    'bird chirp merrily treetop herald arrival new day',
-    'gentle breeze rustle leaf scent bloom flower fill air',
-    'laughter echo park child play game enjoy warm summer afternoon',
-    'distance faint rumble thunder warn approach storm'
-]
-
-PUNCTUATION = [
-    '!!!???!!!????!!!!!!!??????!!!!!!',
-    '!&*()',
-    ',. ;: " ? []',
-    '[].{}()'
-    ''
-]
-
-CLASSES = ['ClassA', 'ClassB', 'ClassC', 'ClassD', 'ClassE']
 
 
 class TestFilterTokens(unittest.TestCase):

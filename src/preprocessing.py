724ecdf72ac45ca6fcb58e046ae346be488daa00
Murad <muradmustafayev03@gmail.com>
2023-08-04 22:36:11 +0400

    added ExtendedTokenizer

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,3 +1,5 @@
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
 import pandas as pd
 import numpy as np
 import spacy
@@ -6,6 +8,33 @@ model_name = "en_core_web_sm"
 if model_name not in spacy.util.get_installed_models():
     spacy.cli.download(model_name)
 nlp = spacy.load(model_name, disable=['parser', 'ner'])
+
+
+class ExtendedTokenizer(Tokenizer):
+    """
+    Extended tokenizer with additional methods for converting texts to padded sequences and back.
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def texts_to_padded_sequences(self, texts, maxlen=None):
+        """
+        Convert texts to padded sequences.
+        :param texts: texts to vectorize
+        :param maxlen: maximum length of the sequences
+        :return: padded sequences
+        """
+        return pad_sequences(self.texts_to_sequences(texts), maxlen=maxlen)
+
+    def padded_sequences_to_text(self, sequences):
+        """
+        Convert padded sequences to texts.
+        :param sequences: sequences to convert
+        :return: texts
+        """
+        cleared_sequences = [list(filter(lambda x: x != 0, row)) for row in sequences]
+        return self.sequences_to_texts(cleared_sequences)
 
 
 def filter_tokens(text: str) -> str:

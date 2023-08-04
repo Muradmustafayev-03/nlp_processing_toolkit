383eb61e0e59df540e0fafb3e988308350c81c5e
Murad <muradmustafayev03@gmail.com>
2023-08-04 23:16:18 +0400

    added TestEnumerateEncodeDecode to preprocessing_test.py

--- tests/preprocessing_test.py
+++ tests/preprocessing_test.py
@@ -69,5 +69,20 @@ class TestOneHotEncodeDecode(unittest.TestCase):
         self.assertTrue(np.equal(self.series2, decoded).all())
 
 
+class TestEnumerateEncodeDecode(unittest.TestCase):
+    series1 = pd.Series([random.choice(CLASSES) for _ in range(1000)])
+    series2 = pd.Series([random.choice(CLASSES) for _ in range(200)])
+
+    def test1(self):
+        encoded = enumerate_encode(self.series1)
+        decoded = enumerate_decode(encoded, self.series2)
+        self.assertTrue(np.equal(self.series1, decoded).all())
+
+    def test2(self):
+        encoded = enumerate_encode(self.series2)
+        decoded = enumerate_decode(encoded, self.series1)
+        self.assertTrue(np.equal(self.series2, decoded).all())
+
+
 if __name__ == '__main__':
     unittest.main()

f59af70de0bd43f769dc6403f50e837a7a4f0b94
Murad <muradmustafayev03@gmail.com>
2023-08-06 13:57:05 +0400

    refactored tests

--- tests/feature_extraction_tests.py
+++ tests/feature_extraction_tests.py
@@ -1,7 +1,6 @@
-from src.preprocessing import *
-from constants import *
+from src.feature_extraction import *
+from test_constants import *
 import unittest
-import random
 
 
 class TestFilterTokens(unittest.TestCase):
@@ -52,37 +51,3 @@ class TestFilterAndExtractLemma(unittest.TestCase):
     def test_punctuation(self):
         for case in PUNCTUATION:
             self.assertEqual('', filter_tokens(case, True))
-
-
-class TestOneHotEncodeDecode(unittest.TestCase):
-    series1 = pd.Series([random.choice(CLASSES) for _ in range(1000)])
-    series2 = pd.Series([random.choice(CLASSES) for _ in range(200)])
-
-    def test1(self):
-        encoded = one_hot_encode(self.series1)
-        decoded = one_hot_decode(encoded, self.series2)
-        self.assertTrue(np.equal(self.series1, decoded).all())
-
-    def test2(self):
-        encoded = one_hot_encode(self.series2)
-        decoded = one_hot_decode(encoded, self.series1)
-        self.assertTrue(np.equal(self.series2, decoded).all())
-
-
-class TestEnumerateEncodeDecode(unittest.TestCase):
-    series1 = pd.Series([random.choice(CLASSES) for _ in range(1000)])
-    series2 = pd.Series([random.choice(CLASSES) for _ in range(200)])
-
-    def test1(self):
-        encoded = enumerate_encode(self.series1)
-        decoded = enumerate_decode(encoded, self.series2)
-        self.assertTrue(np.equal(self.series1, decoded).all())
-
-    def test2(self):
-        encoded = enumerate_encode(self.series2)
-        decoded = enumerate_decode(encoded, self.series1)
-        self.assertTrue(np.equal(self.series2, decoded).all())
-
-
-if __name__ == '__main__':
-    unittest.main()

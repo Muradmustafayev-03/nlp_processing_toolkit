ff03cec0bdbbeb97a29016d2984d81cc3ed311d2
Murad <muradmustafayev03@gmail.com>
2023-08-07 08:20:25 +0400

    added encoding converters

--- src/class_encoders.py
+++ src/class_encoders.py
@@ -41,3 +41,26 @@ def enumerate_decode(encoded: np.array, classes: pd.Series) -> np.array:
     """
     classes = sorted(classes.unique().tolist())
     return np.array([classes[value] for value in encoded])
+
+
+def one_hot_to_enumerate(encoded: np.array) -> np.array:
+    """
+    Converts one-hot encoded array into enumerated array
+    :param encoded: one-hot encoded numpy array, either from one_hot_encode function or from the model output
+    :return: enumerated numpy array
+    """
+    return np.array([np.argmax(one_hot) for one_hot in encoded])
+
+
+def enumerate_to_one_hot(encoded: np.array, classes: pd.Series) -> np.array:
+    """
+    Converts enumerated array into one-hot encoded array
+    :param encoded: encoded 1-D numpy array, either from enumerate_encode function or from the model output
+    :param classes: classes column of the source dataset
+    :return: one-hot encoded numpy array
+    """
+    N = len(classes.unique().tolist())
+    ret = np.zeros((len(encoded), N))
+    for i, value in enumerate(encoded):
+        ret[i, value] = 1
+    return ret
